3dc977428514b166f2e8fc5913c9408cfbb4ba55
Dante Lacey-Thompson <dlacey-thompson@csumb.edu>
2018-03-06 19:05:17 -0800

    adding test for db wrapper functionality v7

--- PolicyWrapper.js
+++ PolicyWrapper.js
@@ -14,7 +14,7 @@ class PolicyWrapper{
 
   //class function
   //function to get user profile information
-  getUserProfileInformation(){
+  this.getUserProfileInformation = function(){
     MongoClient.connect(db_uri, function(err, client){
       if(err){
         throw err;

0ea4d5d5c3c295db3c59d73cc3b3a01fef170fd4
Dante Lacey-Thompson <dlacey-thompson@csumb.edu>
2018-03-07 08:26:09 -0800

    testing profile getter v9 adding profile selection

--- PolicyWrapper.js
+++ PolicyWrapper.js
@@ -18,7 +18,7 @@ PolicyWrapper.prototype.getUserProfileInformation = function(){
       console.log('Successful database connection');
     }
     var db = client.db('aiTestData');
-    db.collection('aiData').find({}, function(err, result){
+    db.collection('aiData').find({}, {profile: 1}, function(err, result){
       if(err){
         throw err;
       }else{
@@ -35,8 +35,10 @@ PolicyWrapper.prototype.getUserProfileInformation = function(){
       client.close();
     });
   });
-
 }
+
+module.exports = PolicyWrapper;
+
 
 // db.collection('users', function(err, collection) {
 //   collection.find({}, function(err, cursor) {
@@ -48,35 +50,3 @@ PolicyWrapper.prototype.getUserProfileInformation = function(){
 //     db.close();
 //   });
 // });
-
-
-module.exports = PolicyWrapper;
-// class PolicyWrapper{
-//   //constructor
-//   constructor(){
-//     this.db_uri = process.env.MONGO_DB_URI;
-//   }
-//
-//   //class function
-//   //function to get user profile information
-//   getUserProfileInformation(){
-//     MongoClient.connect(db_uri, function(err, client){
-//       if(err){
-//         throw err;
-//       }else{
-//         console.log('Successful database connection');
-//       }
-//       var db = client.db('aiTestData');
-//       db.collection('aiData').find({profile: {firstName:1, lastName: 1, emailAddress: 1}}, function(err, result){
-//         if(err){
-//           throw err;
-//         }else{
-//           console.log(result);
-//         }
-//       });
-//     });
-//     client.close();
-//   }
-// }
-
-// module.exports = PolicyWrapper;

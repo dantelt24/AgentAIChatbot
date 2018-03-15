91a60b18c4e9ea7967099c6143d2f6231361eabd
Dante Lacey-Thompson <dlacey-thompson@csumb.edu>
2018-03-14 17:15:42 -0700

    updating profile query response

--- PolicyWrapper.js
+++ PolicyWrapper.js
@@ -30,6 +30,10 @@ PolicyWrapper.prototype.getUserProfileInformation = function(){
         }else{
           console.log(docs);
           console.log(docs.length);
+          var response = 'Thank you for checking in on your profile information.';
+          response += 'The name we have for your profile is ' + docs.profile.firstName + ' ' + docs.profile.lastName;
+          response += ', and the email address on file is ' + docs.profile.emailAddress;
+          console.log(response);
           // console.log(JSON.stringify(docs));
           // for(var i = 0; i < docs.length; i++){
           //   var docObject = docs[i];

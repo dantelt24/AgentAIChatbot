f8725daa9e45ed1606982cf734c07395bcda3687
Dante Lacey-Thompson <dlacey-thompson@csumb.edu>
2018-04-05 20:52:18 -0700

    using update one w/ upsert instead of insert one

--- PolicyWrapper.js
+++ PolicyWrapper.js
@@ -750,45 +750,46 @@ PolicyWrapper.prototype.getExpirationDate = function(callback) {
 
 //messages Collection functions
 
-//set Issues for conversation flow
-// PolicyWrapper.prototype.setCustomerIssue = function(senderInfo, callback){
-//   MongoClient.connect(this.db_uri, function(err, client){
-//     if(err){
-//       throw err;
-//     }
-//     var db = client.db(db_name);
-//     db.collection('messages', function(err, collection){
-//       collection.updateOne({id: senderInfo.id},
-//         {$set: {id: senderInfo.id, 'issue.text': senderInfo.text, 'issue.context': senderInfo.intents, 'issue.solveFlag': false}},
-//         {upsert: true}, function(err, result){
-//           if(err){
-//             throw err;
-//           }else{
-//             console.log(result);
-//             callback(null, result);
-//           }
-//         });
-//     });
-//   });
-// }
-
+// set Issues for conversation flow, updateOne Test
 PolicyWrapper.prototype.setCustomerIssue = function(senderInfo, callback){
   MongoClient.connect(this.db_uri, function(err, client){
     if(err){
-      throw(err);
+      throw err;
     }
     var db = client.db(db_name);
     db.collection('messages', function(err, collection){
-      collection.insertOne({_id: senderInfo.id, issue: {text: senderInfo.issues.text, context: senderInfo.issues.intents, solveFlag: false}}, function(err, result){
-        if(err){
-          throw err;
-        }
-        console.log(result);
-        callback(err, result);
-      });
+      collection.updateOne({id: senderInfo.id},
+        {$set: {_id: senderInfo.id, 'issue.text': senderInfo.text, 'issue.context': senderInfo.intents, 'issue.solveFlag': false}},
+        {upsert: true}, function(err, result){
+          if(err){
+            throw err;
+          }else{
+            console.log(result);
+            callback(null, result);
+          }
+        });
     });
   });
 }
+
+//InsertOne Test
+// PolicyWrapper.prototype.setCustomerIssue = function(senderInfo, callback){
+//   MongoClient.connect(this.db_uri, function(err, client){
+//     if(err){
+//       throw(err);
+//     }
+//     var db = client.db(db_name);
+//     db.collection('messages', function(err, collection){
+//       collection.insertOne({_id: senderInfo.id, issue: {text: senderInfo.issues.text, context: senderInfo.issues.intents, solveFlag: false}}, function(err, result){
+//         if(err){
+//           throw err;
+//         }
+//         console.log(result);
+//         callback(err, result);
+//       });
+//     });
+//   });
+// }
 
 PolicyWrapper.prototype.setIssueSolved = function(senderInfo, callback){
   MongoClient.connect(this.db_uri, function(err, client){
